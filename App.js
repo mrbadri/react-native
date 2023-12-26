3ecbe9acb361a5a541562828ac6ef27245287416
MRBadri <badri0000badri@gmail.com>
2023-12-26 21:52:30 +0330

    [Feat]: practice native component part one

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [number, onChangeNumber] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.content}>{number + count}</Text>
+      <Button onPress={() => setCount(count + 1)} title="+" />
+      <Button onPress={() => setCount(count - 1)} title="-" />
       <StatusBar style="auto" />
+      <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <TextInput />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        // keyboardType="numeric"
+      />
     </View>
   );
 }
@@ -13,8 +36,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
